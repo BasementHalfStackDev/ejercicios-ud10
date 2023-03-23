c7ada997492f988a72da796410565117ebb03b1a
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-23 16:23:58 +0100

    Finalizada calculadora ejercicio 4

--- JPallas_TA10/src/ejercicio_4/Calculator.java
+++ JPallas_TA10/src/ejercicio_4/Calculator.java
@@ -6,23 +6,25 @@ import java.lang.Math;
 
 class Calculator {
 
+	// Attributes
 	double num1;
 	double num2;
 	double result;
 	String operator;
 
+	// Main program
 	void run(Scanner input) {
 		String choice = "";
-
 		do {
+			try {
+				// Asks for operator
+				System.out.print(
+						"Choose an operator: +, -, *, /, pow, sqroot, cubicroot, or type exit to stop the program: ");
+				choice = input.next();
 
-			System.out.print(
-					"Choose an operator: +, -, *, /, pow, sqroot, cubicroot, or type exit to stop the program: ");
-			choice = input.next();
-
-			switch (choice) {
-			case "+":
-				try {
+				// Operates depending on operator
+				switch (choice) {
+				case "+":
 					System.out.print("Enter the first number to sum: ");
 					num1 = input.nextDouble();
 					System.out.print("Enter the second number to sum: ");
@@ -30,11 +32,7 @@ class Calculator {
 					result = num1 + num2;
 					System.out.println(num1 + " + " + num2 + " = " + result);
 					break;
-				} catch (InputMismatchException e) {
-					System.out.println("The introduced input is not of numeric value");
-				}
-			case "-":
-				try {
+				case "-":
 					System.out.print("Enter the first number to substract: ");
 					num1 = input.nextDouble();
 					System.out.print("Enter the second number to substract: ");
@@ -42,59 +40,60 @@ class Calculator {
 					result = num1 - num2;
 					System.out.println(num1 + " - " + num2 + " = " + result);
 					break;
-				} catch (InputMismatchException e) {
-					System.out.println("The introduced input is not of numeric value");
+				case "*":
+					System.out.print("Enter the first number to multiply: ");
+					num1 = input.nextDouble();
+					System.out.print("Enter the second number to multiply: ");
+					num2 = input.nextDouble();
+					result = num1 * num2;
+					System.out.println(num1 + " * " + num2 + " = " + result);
+					break;
+				case "/":
+					System.out.print("Enter the first number to divide: ");
+					num1 = input.nextDouble();
+					System.out.print("Enter the second number to divide: ");
+					num2 = input.nextDouble();
+					if (num1 == 0 || num2 == 0) {
+						throw new ArithmeticException("Cannot divide by 0."); // Throws exception if divided by 0
+					}
+					result = num1 / num2;
+					System.out.println(num1 + " / " + num2 + " = " + result);
+					break;
+				case "pow":
+					System.out.print("Enter the first number to power: ");
+					num1 = input.nextDouble();
+					System.out.print("Power " + num1 + " times: ");
+					num2 = input.nextDouble();
+					result = Math.pow(num1, num2);
+					System.out.println(num1 + " with exponent " + num2 + " = " + result);
+					break;
+				case "sqroot":
+					System.out.print("Enter a number to calculate its square root: ");
+					num1 = input.nextDouble();
+					result = Math.sqrt(num1);
+					System.out.println("The square root of " + num1 + " is " + result);
+					break;
+				case "cubicroot":
+					System.out.print("Enter a number to calculate its cubic root: ");
+					num1 = input.nextDouble();
+					result = Math.cbrt(num1);
+					System.out.println("The square root of " + num1 + " is " + result);
+					break;
+				case "exit":
+					System.out.println("Bye!");
+					break;
+				default:
+					throw new IllegalArgumentException("Invalid operator."); // Throws exception if operator is not
+																				// valid
 				}
-
-			case "*":
-				System.out.print("Enter the first number to multiply: ");
-				num1 = input.nextDouble();
-				System.out.print("Enter the second number to multiply: ");
-				num2 = input.nextDouble();
-				result = num1 * num2;
-				System.out.println(num1 + " * " + num2 + " = " + result);
-				break;
-			case "/":
-				System.out.print("Enter the first number to divide: ");
-				num1 = input.nextDouble();
-				System.out.print("Enter the second number to divide: ");
-				num2 = input.nextDouble();
-				result = num1 / num2;
-				System.out.println(num1 + " / " + num2 + " = " + result);
-				break;
-			case "pow":
-				System.out.print("Enter the first number to power: ");
-				num1 = input.nextDouble();
-				System.out.print("Power " + num1 + " times: ");
-				num2 = input.nextDouble();
-				result = Math.pow(num1, num2);
-				System.out.println(num1 + " with exponent " + num2 + " = " + result);
-				break;
-			case "sqroot":
-				System.out.print("Enter a number to calculate its square root: ");
-				num1 = input.nextDouble();
-				result = Math.sqrt(num1);
-				System.out.println("The square root of " + num1 + " is " + result);
-				break;
-			case "cubicroot":
-				System.out.print("Enter a number to calculate its cubic root: ");
-				num1 = input.nextDouble();
-				result = Math.cbrt(num1);
-				System.out.println("The square root of " + num1 + " is " + result);
-				break;
-			case "exit":
-				System.out.println("Bye!");
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid operator");
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			} catch (InputMismatchException e) {
+				System.out.println("The input must be of numeric value."); // Exception if input is not a number
+				input.next();
+			} catch (ArithmeticException e) {
+				System.out.println(e.getMessage());
 			}
-
 		} while (!choice.equals("exit"));
-
 	}
-	
-	void processInput() {
-		
-	}
-
 }
--- JPallas_TA10/src/ejercicio_4/ejercicio4_App.java
+++ JPallas_TA10/src/ejercicio_4/ejercicio4_App.java
@@ -6,13 +6,17 @@ class ejercicio4_App {
 
 	public static void main(String[] args) {
 
+		// Create input
 		Scanner input = new Scanner(System.in);
-		
+
+		// Create calculator
 		Calculator c1 = new Calculator();
-		
+
+		// Run calculator
 		c1.run(input);
-		
-		
+
+		// Close input
+		input.close();
 
 	}
 
